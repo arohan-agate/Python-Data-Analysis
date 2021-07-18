1e1d5ece0754f8416d918634d3f6379c19d57877
arohan-agate <86753980+arohan-agate@users.noreply.github.com>
2021-07-17 17:05:43 -0700

    Update 1 Create DF.py

--- Visualizing Patterns in Cancer Cells Through Python/1 Create DF.py
+++ Visualizing Patterns in Cancer Cells Through Python/1 Create DF.py
@@ -1,1 +1,4 @@
 df_cancer = pd.DataFrame(np.c_[cancer['data'], cancer['target']], columns = np.append(cancer['feature_names'], ['target']))
+
+# OR...
+df_cancer = pd.read_csv('breast_cancer_cell_data.csv')
