11fddd72508cb8ff9f01d63f75154c79ccde37eb
arohan-agate <86753980+arohan-agate@users.noreply.github.com>
2021-07-18 19:22:53 -0700

    Update 3 Calculate the Derivative (Maximum # of Infections in 1 Day).py

--- COVID-19 Data Analysis/3 Calculate the Derivative (Maximum # of Infections in 1 Day).py
+++ COVID-19 Data Analysis/3 Calculate the Derivative (Maximum # of Infections in 1 Day).py
@@ -1,1 +1,8 @@
-
+# Max # of infections in 1 day
+df_covid.loc['China'].diff().max()
+df_covid.loc['Italy'].diff().max()
+df_covid.loc['US'].diff().max()
+df_covid.loc['India'].diff().max()
+df_covid.loc['Australia'].diff().max()
+df_covid.loc['Brazil'].diff().max()
+df_covid.loc['Kenya'].diff().max()
