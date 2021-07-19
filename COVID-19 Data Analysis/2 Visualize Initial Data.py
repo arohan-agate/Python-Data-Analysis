fb6ef055d70478609a255a720d3beb56bfb5dfe3
arohan-agate <86753980+arohan-agate@users.noreply.github.com>
2021-07-18 19:19:50 -0700

    Create 2 Visualize Initial Data.py

---
+++ COVID-19 Data Analysis/2 Visualize Initial Data.py
@@ -0,0 +1,8 @@
+df_covid.loc['China'].plot()
+df_covid.loc['Italy'].plot()
+df_covid.loc['US'].plot()
+df_covid.loc['India'].plot()
+df_covid.loc['Australia'].plot()
+df_covid.loc['Brazil'].plot()
+df_covid.loc['Kenya'].plot()
+plt.legend()
